9cdda5488b9ed37b162d7e8a1e9ba90e8560dd17
YulyaKukareko <YulyaKukareko>
2019-05-16 17:04:05 +0300

    Change config files project

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,12 +2,17 @@ let path = require('path');
 
 module.exports = {
     entry: "./src/index.js",
+    devtool: 'eval-source-map',
     output: {
         path: path.resolve(__dirname, '../server/src/main/webapp/resources'),
         publicPath: './',
         filename: "bundle.js",
     },
     watch: true,
+    watchOptions: {
+        poll: true,
+        ignored: /node_modules/
+    },
     module: {
         rules: [
             {
@@ -17,6 +22,14 @@ module.exports = {
                 options: {
                     presets: ["@babel/preset-env", "@babel/preset-react"]
                 }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|gif|woff2)$/,
+                use: ["url-loader"]
             }
         ]
     }
